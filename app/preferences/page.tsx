feeed68f38980d5cb3dd707abb5a4f349ed9470f
michael-norman7 <mnorman0801@gmail.com>
2024-04-16 17:08:34 -0500

    added to preferences page

--- app/preferences/page.tsx
+++ app/preferences/page.tsx
@@ -1,11 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button, Input } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Preferences() {
   return (
-    <div className="bg-slate-200">
-      <h1 className="text-center text-3xl py-4">Preferences Page</h1>
+    <div className="flex justify-center">
+      <div className="space-y-4 py-12 w-1/2">
+        <h1 className="text-xl font-semibold">How it Works</h1>
+        <p>
+          To provide more accurate recommendations for you, please rate these
+          sample clothes to determine your preferences. Or you can select one of
+          our User presets!
+        </p>
+      </div>
+
+      {/* <div className="flex justify-end mb-4">
+        <Select onValueChange={(value) => setSelectedProfile(value)}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Profile" />
+          </SelectTrigger>
+          <SelectContent>
+            {profiles.map((profile) => (
+              <SelectItem
+                value={profile}
+                onSelect={() => {
+                  setSelectedProfile(profile);
+                }}>
+                {profile}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div> */}
     </div>
   );
 }
